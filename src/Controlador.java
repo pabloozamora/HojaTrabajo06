49250044ee524df9a6dc42d2d29557e9d3dd75ad
pabloozamora <pabloozamora02@gmail.com>
2022-03-21 22:35:28 -0600

    Implementación completa
    Clase Main finalizada y Controlador completamente funcional con las 3 implementaciones MAP

--- src/Controlador.java
+++ src/Controlador.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.Map.Entry;
 
 public class Controlador {
 
@@ -11,33 +12,55 @@ public class Controlador {
 		for (int i = 0; i < categories.size();i++) {
 			inventory.put(products.get(i),categories.get(i));
 		}
+		MapFactory<String, String> factory2 = new MapFactory<String, String>(implementation);		
+		cart = factory2.getInstance();
 	}
 	
-	public void addProduct(String category, String product, int quantity) {
-		for (int i = 1; i <= quantity;i++) {
-			int count = 1;
-			String current_product = product + count;
-			if (!cart.containsKey(current_product)) {//Es un producto nuevo
-				cart.put(current_product, category);
+	public String addProduct(String category, String product, int quantity) {
+		String message = "";
+		if (inventory.containsValue(category)) {
+			if (inventory.containsKey(product)) {
+				if (inventory.get(product).equals(category)) {
+					for (int i = 1; i <= quantity;i++) {
+						int count = 1;
+						String current_product = product + count;
+						if (!cart.containsKey(current_product)) {//Es un producto nuevo
+							cart.put(current_product, category);
+						}
+						else {
+							while (cart.containsKey(current_product)) {
+								count++;
+								current_product = product + count;
+							}
+							cart.put(current_product, category);
+						}
+					}
+					message = "\nProducto(s) agregado(s) al carrito con exito";
+				}
+				else {
+					message = "\nEl producto y la categoria ingresados no coinciden";
+					return message;
+				}
 			}
 			else {
-				while (cart.containsKey(current_product)) {
-					count++;
-					current_product = product + count;
-				}
-				cart.put(current_product, category);
+				message = "\nEl producto ingresado no es valido";
+				return message;
 			}
-			
 		}
+		else {
+			message = "\nLa categoria ingresada no es valida (Revise mayusculas y espacios)";
+			return message;
+		}
+		return message;
 	}
 	
 	
 	public String showCategory(String product) {
 		String message = "";
 		String search_product = product + "1";
-		String category = (String) cart.get(search_product);
+		String category = (String) inventory.get(search_product);
 		if (category == null) {
-			message = "\nEl producto todavia no ha sido agregado al carrito";
+			message = "\nEl producto ingresado no es valido";
 		}
 		else {
 			message = "\nEl producto " + product + " pertenece a la categoria " + category;
@@ -46,51 +69,67 @@ public class Controlador {
 	}
 	
 	public String showCart() {
-		if (!cart.isEmpty()) {
+		if (cart.isEmpty()) {
 			return "\nEl carrito de compras todavia esta vacio";
 		}
 		String message = "";
+		ArrayList<String> alreadyDone = new ArrayList<String>();
 		for (Map.Entry<String, String> entry : cart.entrySet()) {
 			String product = entry.getKey();
-			String temp_product = product + 1;
-			boolean notFinished = true;
-			int quantity = 0;
-			while (notFinished) {
-				temp_product = product + (quantity+1);
-				if(cart.containsKey(temp_product)) {
-					quantity++;
+			int length = product.length();
+			product = product.substring(0,length-1);
+			if (!alreadyDone.contains(product)) {
+				String temp_product = product + 1;
+				boolean notFinished = true;
+				int quantity = 0;
+				while (notFinished) {
+					temp_product = product + (quantity+1);
+					if(cart.containsKey(temp_product)) {
+						quantity++;
+					}
+					else {
+						notFinished = false;
+					}
 				}
-				else {
-					notFinished = false;
-				}
+				
+				alreadyDone.add(product);
+				temp_product = product + 1;
+				message += "\nProducto: " + product + "\nCategoria: " + cart.get(temp_product) + "\nCantidad: " + quantity + "\n";
 			}
-			
-			message += "\nProducto: " + product + "\nCategoria: " + cart.get(temp_product) + "\nCantidad: " + quantity + "\n"; 
+			 
 		}
 		return message;
 	}
 	
-	public String showCartbyCategory() {
-		if (!cart.isEmpty()) {
+	public String showCartbyType() {
+		if (cart.isEmpty()) {
 			return "\nEl carrito de compras todavia esta vacio";
 		}
 		String message = "";
+		ArrayList<String> alreadyDone = new ArrayList<String>();
 		for (Map.Entry<String, String> entry : cart.entrySet()) {
 			String product = entry.getKey();
-			String temp_product = product + 1;
-			boolean notFinished = true;
-			int quantity = 0;
-			while (notFinished) {
-				temp_product = product + (quantity+1);
-				if(cart.containsKey(temp_product)) {
-					quantity++;
+			int length = product.length();
+			product = product.substring(0,length-1);
+			if (!alreadyDone.contains(product)) {
+				String temp_product = product + 1;
+				boolean notFinished = true;
+				int quantity = 0;
+				while (notFinished) {
+					temp_product = product + (quantity+1);
+					if(cart.containsKey(temp_product)) {
+						quantity++;
+					}
+					else {
+						notFinished = false;
+					}
 				}
-				else {
-					notFinished = false;
-				}
+				
+				alreadyDone.add(product);
+				temp_product = product + 1;
+				message += "\nCategoria: " + cart.get(temp_product) + "\nProducto: " + product + "\nCantidad: " + quantity + "\n";
 			}
-			
-			message += "\nCategoria: " + "\nProducto: " + product + cart.get(temp_product) + "\nCantidad: " + quantity + "\n"; 
+			 
 		}
 		return message;
 	}
@@ -99,8 +138,20 @@ public class Controlador {
 		String message = "";
 		for (Map.Entry<String, String> entry : inventory.entrySet()) {
 			String product = entry.getKey();
-			message += "\nCategoria: " + "\nProducto: " + product + inventory.get(product) + "\n";
+			message += "\nCategoria: " + inventory.get(product) + "\nProducto: " + product + "\n";
 		}
 		return message;
 	}
+	
+	public String showInventorybyType() {
+		String message = "";
+		List<Map.Entry<String, String>> list = new ArrayList<>(inventory.entrySet());
+		list.sort(Entry.comparingByValue());
+		for (Map.Entry<String, String> entry: list) {
+			String product = entry.getKey();
+			message += "\nCategoria: " + inventory.get(product) + "\nProducto: " + product + "\n";
+		}
+		return message;
+	}
+	
 }
--- src/Main.java
+++ src/Main.java
@@ -53,10 +53,62 @@ public class Main {
 		for (String row : content) {
 			String[] line = row.split("\\|");
 			String category = line[0].replaceAll("\\s+$", "");
-			String product = line[1];
+			String product = line[1].replaceAll("\t", "");
 			categories.add(category);
 			products.add(product);
 		}
+		String imp_menu = "\nSeleccione la implementacion de MAP a utilizar:\n1. Hashmap\n2. TreeMap\n3. LinkedHashMap";
+		int implementation = getValidInt(sc, imp_menu, false, 1,2,3);
+		controlador.createInventory(categories, products, implementation);
 		
+		String main_menu = "\nBienvenido a nuestra tienda online. Seleccione la accion a realizar:\n1. Agregar un producto al carrito" +
+		"\n2. Mostrar la categoria de un producto\n3. Mostrar el carrito\n4. Mostrar el carrito, ordenado por categoria\n5. Mostrar el inventario" +
+				"\n6. Mostrar el inventario, ordenado por categoria\n7. Salir";
+		boolean end = false;
+		while(!end) {
+			int option = getValidInt(sc, main_menu, false, 1,2,3,4,5,6,7);
+			switch (option) {
+			case 1:
+				System.out.println("\nIngrese la categoria del producto a agregar:");
+				String category = sc.nextLine();
+				System.out.println("\nIngrese el producto a ingresar:");
+				String product = sc.nextLine();
+				System.out.println("\nIngrese la cantidad a ingresar:");
+				int quantity = sc.nextInt();
+				sc.nextLine();
+				System.out.println(controlador.addProduct(category, product, quantity));
+				break;
+				
+			case 2:
+				System.out.println("\nIngrese el nombre del producto:");
+				String check_product = sc.nextLine();
+				System.out.println(controlador.showCategory(check_product));
+				break;
+				
+			case 3:
+				System.out.println("\nMostrando el carrito: ");
+				System.out.println(controlador.showCart());
+				break;
+				
+			case 4:
+				System.out.println("\nMostrando el carrito por tipo: ");
+				System.out.println(controlador.showCartbyType());
+				break;
+				
+			case 5:
+				System.out.println("\nMostrando el inventario: ");
+				System.out.println(controlador.showInventory());
+				break;
+			
+			case 6:
+				System.out.println("\nMostrando el inventario por tipo: ");
+				System.out.println(controlador.showInventorybyType());
+				break;
+				
+			case 7:
+				System.out.println("\nGracias por utilizar nuestros servicios en linea. Feliz dia");
+				end = true;
+			}
+		}
 	}
 }
